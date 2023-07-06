5ae577a2300197bd2467c1a8d960e6a0f0b32f66
jurichar <jurichar@student.42.fr>
2023-07-06 20:01:26 +0200

    change to swift

---
+++ ft_hangouts/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ft_hangouts
+//
+//  Created by Julien Richard on 06/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello C'est Ft_hangouts")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ft_hangouts/ft_hangoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ft_hangoutsApp.swift
+//  ft_hangouts
+//
+//  Created by Julien Richard on 06/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ft_hangoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
